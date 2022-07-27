c683f234bac2fc4ec2302b881272833fb275e844
Jocelyn Naarden <tzionone@gmail.com>
2022-07-26 23:14:31 -0400

    Finished Assignment ✔

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 4, 'M', 'Brown', 'Friendly', 1),
+('Rowdy',12, 'F', 'Green', 'Grumpy', 1),
+('Tabitha', 5, 'M', 'White & Black', 'Shy', 0),
+('Sergeant Brown', 1, 'F', 'Orange', 'Scared', 1),
+('Melissa9', 8, 'F', 'Orange & Brown', 'Outgoing', 1),
+('Grinch', 10, 'M', 'Purple', 'Playful', 1),
+('Wendy', 14, 'F', 'Brown', 'Mean', 1),
+(NULL, 14, 'F', 'Brown', 'Rude', 0);
